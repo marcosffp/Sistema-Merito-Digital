af1b57a2b0766e31c20679bd8c50b85b4d266bb5
alvimdev <alvim103@gmail.com>
2025-10-20 09:59:22 -0300

    feat: Initialize Spring Boot project with Maven Wrapper + Fixing UML issues

---
+++ implementacao/src/main/java/com/projeto/lab/implementacao/Main.java
@@ -0,0 +1,13 @@
+package com.projeto.lab.implementacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
---
+++ implementacao/src/test/java/com/projeto/lab/implementacao/ImplementacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.lab.implementacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImplementacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
