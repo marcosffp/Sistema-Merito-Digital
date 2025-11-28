7a3da485baad7b11e92aa4f02f90d37cfc0cbb17
marcosffp <mafpinto@sga.pucminas.br>
2025-11-28 16:55:57 -0300

    Add communication diagram for VantagemController and VantagemService

--- implementacao/api/src/main/java/com/projeto/lab/implementacao/service/AlunoService.java
+++ implementacao/api/src/main/java/com/projeto/lab/implementacao/service/AlunoService.java
@@ -1,25 +1,21 @@
 package com.projeto.lab.implementacao.service;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.projeto.lab.implementacao.dto.AlunoCompletoResponse;
 import com.projeto.lab.implementacao.dto.AlunoRequest;
 import com.projeto.lab.implementacao.dto.AlunoResumoResponse;
 import com.projeto.lab.implementacao.dto.AlunoUpdateRequest;
-import com.projeto.lab.implementacao.dto.AlunoCompletoResponse;
-import com.projeto.lab.implementacao.dto.TransacaoRecebimentoDTO;
 import com.projeto.lab.implementacao.exception.AlunoException;
-import com.projeto.lab.implementacao.model.Aluno;
-import com.projeto.lab.implementacao.model.Instituicao;
-import com.projeto.lab.implementacao.model.Resgate;
-import com.projeto.lab.implementacao.model.Distribuicao;
-import com.projeto.lab.implementacao.repository.AlunoRepository;
 import com.projeto.lab.implementacao.mapper.AlunoMapper;
+import com.projeto.lab.implementacao.model.Aluno;
+import com.projeto.lab.implementacao.model.Distribuicao;
+import com.projeto.lab.implementacao.model.Instituicao;
+import com.projeto.lab.implementacao.repository.AlunoRepository;
 import com.projeto.lab.implementacao.repository.DistribuicaoRepository;
 
 import lombok.RequiredArgsConstructor;
